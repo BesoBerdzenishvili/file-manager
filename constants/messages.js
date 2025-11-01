80b2cb47aea7fa3ba187278eaa174dbc90db6501
fliderman01 <fliderman01@gmail.com>
2025-11-01 13:08:09 +0400

    feat: navigation

--- constants/messages.js
+++ constants/messages.js
@@ -4,7 +4,7 @@ const messages = {
     goodbye: (username) =>
       `Thank you for using File Manager, ${username}, goodbye!`,
     current_dir: (path_to_working_directory) =>
-      `You are currently in ${path_to_working_directory}!`,
+      `You are currently in ${path_to_working_directory}`,
   },
   error: {
     invalid_input: "Invalid input",
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import readline from "node:readline";
 import messages from "./constants/messages.js";
 import parseArgs from "./utils/parseArgs.js";
+import { Navigation } from "./controllers/navigation.controller.js";
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
+const nav = new Navigation();
 
 function expense_tracker() {
   rl.question(`File-manager: `, (command) => {
@@ -13,10 +16,20 @@ function expense_tracker() {
       case ".exit":
         rl.close();
         break;
+      case "up":
+        nav.up();
+        break;
+      case "cd":
+        nav.cd(command.split(" ")[1]);
+        break;
+      case "ls":
+        nav.ls();
+        break;
       default:
         console.log(messages.error.operation_failed);
         break;
     }
+    console.log(messages.info.current_dir(process.cwd()));
     expense_tracker();
   });
 }
